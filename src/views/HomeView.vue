59749c9fad5720def3fc6d1bc4c662a31efc9709
Gregh <130164023+Gregh-bit@users.noreply.github.com>
2025-06-05 12:51:58 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <main
-    class="mt-10 md:mt-1 flex flex-col-reverse gap-8 items-center md:flex-row md:gap-16 md:justify-center min-h-[65vh] md:min-h-[80vh]">
-    <div class="space-y-2 text-center md:text-left px-10">
+    class="relative mt-10 md:mt-1 flex flex-col-reverse gap-8 items-center md:flex-row md:gap-16 md:justify-center min-h-[65vh] md:min-h-[80vh]">
+    
+    <div class="space-y-2 text-center md:text-left px-10 z-10">
       <p class="text-amber-200">Ciao, sono</p>
       <h1 class="text-4xl font-bold md:text-5xl text-white fadein-up">Gregorio Cinque</h1>
       <div class="py-2">
@@ -14,17 +15,23 @@
       <p class="text-white pr-4 fade-in-from-left">Benvenuto nel mio sito personale. <span class="wave">👋🏼</span></p>
       <br>
     </div>
-    <div class="flex justify-center md:justify-start fadein-right">
-      <img alt="avatar" fetchpriority="high" width="300" height="300" decoding="async" data-nimg="1"
-        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="https://i1.sndcdn.com/avatars-000214125831-5q6tdw-t500x500.jpg">
-    </div>
-  </main>
-</template>
 
-<template>
-  <div class="relative w-full h-[600px]">
-    <Orb :hue="0.7" :hoverIntensity="0.4" />
-  </div>
+    <div class="relative w-[300px] h-[300px] fadein-right z-10">
+      <canvas ref="orbCanvas" class="absolute inset-0 w-full h-full rounded-full overflow-hidden"></canvas>
+      <img
+        alt="avatar"
+        fetchpriority="high"
+        width="300"
+        height="300"
+        decoding="async"
+        class="w-full h-full object-cover rounded-full"
+        src="https://i1.sndcdn.com/avatars-000214125831-5q6tdw-t500x500.jpg"
+      />
+    </div>
+
+    <!-- Orb Background Canvas -->
+    <Orb class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[360px] h-[360px] z-0" :hue="0.7" :hoverIntensity="0.4" />
+  </main>
 </template>
 
 <script>
@@ -32,7 +39,12 @@ import Orb from '@/components/Orb.vue'
 
 export default {
   name: 'HomeView',
-  components: { Orb }
+  components: { Orb },
+  data() {
+    return {
+      txt: 'Sviluppatore Frontend'
+    }
+  }
 }
 </script>
 
